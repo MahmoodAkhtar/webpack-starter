d4b2a2c4c006cd8d9f136f6070434ca02e4e94e0
Mahmood Akhtar <mahmood233@hotmail.com>
2018-12-16 15:02:14 +0000

    Added a shadow dom root to web component.

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -1,7 +1,12 @@
 class BlogPost extends HTMLElement {
 
+    constructor() {
+        super();
+        this.root = this.attachShadow({ mode: "open" });
+    }
+
     set blogPost(blogPost) {
-        this.innerHTML = `
+        this.root.innerHTML = `
             <a href="blog-posts/${blogPost.id}">${blogPost.title}</a>
             <p>${blogPost.body}</p>
         `;
